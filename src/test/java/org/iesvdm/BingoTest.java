9b5c2be30b88af5fe76427f8f3a46de006cfa03c
Neklof1982 <neklof1982@hotmail.com>
2024-04-02 12:14:11 +0200

    clase

---
+++ src/test/java/org/iesvdm/BingoTest.java
@@ -0,0 +1,44 @@
+package org.iesvdm;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BingoTest {
+
+    @Test
+    void whenBooleanIsTrue(){
+
+        //Monadas, encadenamiento de metodos
+        //este metodo es mucho más declarativo
+        boolean flag = true;
+        assertThat(flag).isTrue();
+    }
+    @Test
+    void whenBooleanIsFalse() {
+        //Monadas, encadenamiento de metodos
+        // este metodo es mucho más declarativo
+        boolean flag =false;
+        assertThat(flag).isFalse();
+    }
+
+    @Test
+    void rellenarNumerosCarton (){
+
+    }
+
+    @Test
+    void ponerBlancos(){
+
+    }
+
+    @Test
+    void buscarFila(){
+
+    }
+
+}
