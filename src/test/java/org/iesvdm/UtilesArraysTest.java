9f2d5a6c33166e2aca2d78272a7d5c838fce17bc
Neklof1982 <neklof1982@hotmail.com>
2024-04-05 21:34:03 +0200

    Insertar windows en Ubuntu prueba

---
+++ src/test/java/org/iesvdm/UtilesArraysTest.java
@@ -0,0 +1,107 @@
+package org.iesvdm;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UtilesArraysTest {
+
+    @Test
+    void limpiar(){
+
+    }
+
+    @Test
+    void rellenar(){
+
+    }
+
+    @Test
+    void insertarAlFinal(){
+
+    }
+
+    @Test
+    void insertarAlPrincipio(){
+
+    }
+
+    @Test
+    void insertarEnPosicion(){
+
+    }
+
+    @Test
+    void insertarOrdenado(){
+
+    }
+
+    @Test
+    void eliminarUltimo(){
+
+    }
+
+    @Test
+    void eliminarPrimero(){
+
+    }
+
+    @Test
+    void eliminarPosicion(){
+
+    }
+
+    @Test
+    void eliminar(){
+
+    }
+
+    @Test
+    void ordenar(){
+
+    }
+
+    @Test
+    void desordenar(){
+
+    }
+
+    @Test
+    void invertir(){
+
+    }
+
+    @Test
+    void imprimir(){
+
+    }
+
+    @Test
+    void  buscar(){
+
+    }
+
+    @Test
+    void partirPor(){
+
+    }
+
+    @Test
+    void sonIguales(){
+
+    }
+
+    @Test
+    void elementosIguales(){
+
+    }
+
+    @Test
+    void concatenarArrays(){
+
+    }
+
+}
